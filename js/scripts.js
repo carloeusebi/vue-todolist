d550aaba9c2ebe8ed16d2b814f658ba9cf7a2cfc
carloeusebi <carloeusebi@gmail.com>
2023-06-20 14:52:40 +0200

    Completed add task function

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     data() {
         return {
             searchedTask: '',
+            newTask: '',
             tasks: [
                 {
                     id: 1,
@@ -49,6 +50,10 @@ const app = Vue.createApp({
     },
 
     computed: {
+        nextId() {
+            let highestId = this.tasks.reduce((highest, { id }) => id > highest ? id : highest, 0);
+            return ++highestId;
+        },
         filteredTasks() {
             const searched = this.searchedTask.trim().toLowerCase();
             return this.tasks.filter(({ text }) => text.toLowerCase().includes(this.searchedTask));
@@ -58,13 +63,31 @@ const app = Vue.createApp({
         },
         toDoTasks() {
             return this.filteredTasks.filter(task => !task.done);
-        }
+        },
 
     },
 
     methods: {
         deleteTask(taskToDeleteId) {
             this.tasks = this.tasks.filter(({ id }) => id !== taskToDeleteId)
+        },
+        addTask() {
+            const text = this.newTask.trim();
+            this.newTask = '';
+
+            if (!text) return;
+
+            const id = this.nextId;
+
+            const newTask = {
+                id,
+                text,
+                done: false
+            }
+
+            this.tasks.push(newTask);
+
+            this.$refs.addInput.focus();
         }
     }
 
